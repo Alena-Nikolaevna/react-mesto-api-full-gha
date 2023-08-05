73941e07a228071af44840e6722dd7c4c03643bb
Alena Nikolaevna <magnittyt@mail.ru>
2023-08-06 00:04:50 +0700

    ПР-15

--- backend/middlewares/validations.js
+++ backend/middlewares/validations.js
@@ -16,7 +16,7 @@ const signupValidation = celebrate({ // createUser
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(regexUrl),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required().min(6),
   }),
 });
 
